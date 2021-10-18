d463a12ee657af05a85ee503ad0e108070ed1bf5
DELL <DELL@SEMIH>
2021-10-18 10:14:07 +0300

    Add project files.

---
+++ ASPNetCoreIntro/Controllers/CustomerController.cs
@@ -0,0 +1,51 @@
+﻿using ASPNetCoreIntro.Entities;
+using ASPNetCoreIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreIntro.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index2() //Product Controllerinin Index2 Aksiyonunun Viewi
+        {
+            List<Customer> customers = new List<Customer>
+            {
+                new Customer{Id=1,FirstName="Semih",LastName="Kaya",City="Istanbul"},
+                new Customer{Id=1,FirstName="Sude",LastName="Öztürk",City="Ankara"},
+                new Customer{Id=1,FirstName="Muhsin",LastName="Keskin",City="Trabzon"}
+            };
+
+            List<string> shops = new List<string> { "Ankara", "İstanbul", "Antalya" };
+            var model = new CustomerListViewModel
+            {
+                Customers = customers,
+                Shops = shops
+            };
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult SaveCustomer()
+        {
+            return View(new SaveCustomerViewModel { 
+                Cities = new List<SelectListItem>
+                {
+                    new SelectListItem {Text="Ankara",Value="06"},
+                    new SelectListItem {Text="İstanbul",Value="34"},
+                    new SelectListItem {Text="İzmir",Value="35"}
+                }
+            });
+        }
+
+        [HttpPost]
+        public string SaveCustomer(Customer customer)
+        {
+            return "Kaydedildi";
+        }
+    }
+}
---
+++ ASPNetCoreIntro/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using ASPNetCoreIntro.Entities;
+using ASPNetCoreIntro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreIntro.Controllers
+{
+    //HTTP GET İŞLEMLERİ  
+    public class ProductController : Controller
+    {
+        //Controller içindeki public methodlar = Aksiyon-Action  //Index fonksiyonu = https:// localhost...:product/index
+        public IActionResult Index() //Product Controllerinin Index Aksiyonunun Viewi
+        {
+            return View();
+        }
+    }
+}
